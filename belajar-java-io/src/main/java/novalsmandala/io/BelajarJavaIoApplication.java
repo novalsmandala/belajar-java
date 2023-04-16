688db16cc93aafbb8c0362dda72876c4281a231e
Noval Surya Mandala <78200010+novalsmandala@users.noreply.github.com>
2023-04-16 18:59:11 +0700

    add java io

---
+++ belajar-java-io/src/main/java/novalsmandala/io/BelajarJavaIoApplication.java
@@ -0,0 +1,12 @@
+package novalsmandala.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaIoApplication.class, args);
+	}
+}
